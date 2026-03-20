b7d424b0f9f6395387c6cd73df62b397bc8927a8
Ngô Quốc Đạt <datlechin@gmail.com>
2026-03-20 12:36:46 +0700

    fix: remove unrelated DynamoDB type from PR

--- TablePro/Models/Connection/DatabaseConnection.swift
+++ TablePro/Models/Connection/DatabaseConnection.swift
@@ -237,7 +237,6 @@ extension DatabaseType {
     static let scylladb = DatabaseType(rawValue: "ScyllaDB")
     static let etcd = DatabaseType(rawValue: "etcd")
     static let cloudflareD1 = DatabaseType(rawValue: "Cloudflare D1")
-    static let dynamodb = DatabaseType(rawValue: "DynamoDB")
 }
 
 extension DatabaseType: Codable {
