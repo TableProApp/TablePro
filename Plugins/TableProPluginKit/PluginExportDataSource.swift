97503fd955f7fe11bec7e1b74c19d575621fadf8
Ngo Quoc Dat <datlechin@gmail.com>
2026-05-08 20:18:29 +0700

    feat(plugins): bulk fetchAllColumns/fetchAllForeignKeys on PluginExportDataSource (#1114)

--- Plugins/TableProPluginKit/PluginExportDataSource.swift
+++ Plugins/TableProPluginKit/PluginExportDataSource.swift
@@ -11,12 +11,16 @@ public protocol PluginExportDataSource: AnyObject, Sendable {
     func fetchDependentSequences(table: String, databaseName: String) async throws -> [PluginSequenceInfo]
     func fetchDependentTypes(table: String, databaseName: String) async throws -> [PluginEnumTypeInfo]
     func fetchColumns(table: String, databaseName: String) async throws -> [PluginColumnInfo]
+    func fetchAllColumns(databaseName: String) async throws -> [String: [PluginColumnInfo]]
     func fetchForeignKeys(table: String, databaseName: String) async throws -> [PluginForeignKeyInfo]
+    func fetchAllForeignKeys(databaseName: String) async throws -> [String: [PluginForeignKeyInfo]]
 }
 
 public extension PluginExportDataSource {
     func fetchDependentSequences(table: String, databaseName: String) async throws -> [PluginSequenceInfo] { [] }
     func fetchDependentTypes(table: String, databaseName: String) async throws -> [PluginEnumTypeInfo] { [] }
     func fetchColumns(table: String, databaseName: String) async throws -> [PluginColumnInfo] { [] }
+    func fetchAllColumns(databaseName: String) async throws -> [String: [PluginColumnInfo]] { [:] }
     func fetchForeignKeys(table: String, databaseName: String) async throws -> [PluginForeignKeyInfo] { [] }
+    func fetchAllForeignKeys(databaseName: String) async throws -> [String: [PluginForeignKeyInfo]] { [:] }
 }
--- TablePro/Core/Plugins/ExportDataSourceAdapter.swift
+++ TablePro/Core/Plugins/ExportDataSourceAdapter.swift
@@ -73,11 +73,25 @@ final class ExportDataSourceAdapter: PluginExportDataSource, @unchecked Sendable
         return try await pluginDriver.fetchColumns(table: table, schema: pluginDriver.currentSchema)
     }
 
+    func fetchAllColumns(databaseName: String) async throws -> [String: [PluginColumnInfo]] {
+        guard let pluginDriver = (driver as? PluginDriverAdapter)?.schemaPluginDriver else {
+            return [:]
+        }
+        return try await pluginDriver.fetchAllColumns(schema: pluginDriver.currentSchema)
+    }
+
     func fetchForeignKeys(table: String, databaseName: String) async throws -> [PluginForeignKeyInfo] {
         guard let pluginDriver = (driver as? PluginDriverAdapter)?.schemaPluginDriver else {
             return []
         }
         return try await pluginDriver.fetchForeignKeys(table: table, schema: pluginDriver.currentSchema)
+    }
+
+    func fetchAllForeignKeys(databaseName: String) async throws -> [String: [PluginForeignKeyInfo]] {
+        guard let pluginDriver = (driver as? PluginDriverAdapter)?.schemaPluginDriver else {
+            return [:]
+        }
+        return try await pluginDriver.fetchAllForeignKeys(schema: pluginDriver.currentSchema)
     }
 
     // MARK: - Helpers
