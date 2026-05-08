71e0e4b78f43fc35a3b356e606e48ccb937fcf9c
Ngô Quốc Đạt <datlechin@gmail.com>
2026-05-08 18:55:29 +0700

    refactor: extract QueryExecutionCoordinator from MainContentCoordinator

--- TablePro/Views/Main/MainContentCoordinator.swift
+++ TablePro/Views/Main/MainContentCoordinator.swift
@@ -100,6 +100,7 @@ final class MainContentCoordinator {
     }()
 
     @ObservationIgnored private(set) var filterCoordinator: FilterCoordinator!
+    @ObservationIgnored private(set) var queryExecutionCoordinator: QueryExecutionCoordinator!
 
     /// Stable identifier for this coordinator's window (set by MainContentView on appear)
     var windowId: UUID?
@@ -395,6 +396,7 @@ final class MainContentCoordinator {
             }
 
         self.filterCoordinator = FilterCoordinator(parent: self)
+        self.queryExecutionCoordinator = QueryExecutionCoordinator(parent: self)
 
         Self.lifecycleLogger.info(
             "[open] MainContentCoordinator.init done connId=\(connection.id, privacy: .public) elapsedMs=\(Int(Date().timeIntervalSince(initStart) * 1_000))"
