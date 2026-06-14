ca497e4ac7b720cdf4fe30aef059c2fec32d6ddf
Ngo Quoc Dat <datlechin@gmail.com>
2026-06-14 23:26:11 +0700

    refactor(sidebar): match popover database search to tree fuzzy matching

--- TablePro/Views/Sidebar/DatabaseTreeFilterPopover.swift
+++ TablePro/Views/Sidebar/DatabaseTreeFilterPopover.swift
@@ -22,7 +22,7 @@ struct DatabaseTreeFilterPopover: View {
 
     private var matchingDatabases: [DatabaseMetadata] {
         guard !searchText.isEmpty else { return selectableDatabases }
-        return selectableDatabases.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        return selectableDatabases.filter { FuzzyMatcher.matches(query: searchText, candidate: $0.name) }
     }
 
     private var shownCount: Int {
