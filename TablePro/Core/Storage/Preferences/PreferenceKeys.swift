69dd1f1be1deec3c140003c2c02b74a5cd9e77ed
Ngo Quoc Dat <datlechin@gmail.com>
2026-07-12 00:35:15 +0700

    refactor(datagrid): route the row inspector JSON field height through PreferenceKeys

--- TablePro/Core/Storage/Preferences/PreferenceKeys.swift
+++ TablePro/Core/Storage/Preferences/PreferenceKeys.swift
@@ -9,11 +9,13 @@ enum PreferenceKeys {
     static let linkedFolders = DefaultsKey<[LinkedFolder]>("com.TablePro.linkedFolders")
     static let linkedSQLFolders = DefaultsKey<[LinkedSQLFolder]>("com.TablePro.linkedSQLFolders")
     static let selectedSettingsPane = DefaultsKey<String>("com.TablePro.settings.selectedPane")
+    static let rowInspectorJsonFieldHeight = DefaultsKey<Double>("com.TablePro.rightSidebar.jsonFieldHeight")
 
     static let registeredKeyNames: [String] = [
         linkedFolders.name,
         linkedSQLFolders.name,
         selectedSettingsPane.name,
+        rowInspectorJsonFieldHeight.name,
     ]
 
     static func columnDisplayFormats(_ scope: TableScope) -> DefaultsKey<[String: ValueDisplayFormat]> {
--- TableProTests/Core/Storage/AppSettingsStorageResetTests.swift
+++ TableProTests/Core/Storage/AppSettingsStorageResetTests.swift
@@ -15,10 +15,12 @@ struct AppSettingsStorageResetTests {
         let storage = AppSettingsStorage(userDefaults: defaults)
         defaults.set("account", forKey: PreferenceKeys.selectedSettingsPane.name)
         defaults.set("tree", forKey: SidebarPersistenceKey.defaultLayout)
+        defaults.set(320.0, forKey: PreferenceKeys.rowInspectorJsonFieldHeight.name)
 
         storage.resetToDefaults()
 
         #expect(defaults.string(forKey: PreferenceKeys.selectedSettingsPane.name) == nil)
         #expect(defaults.string(forKey: SidebarPersistenceKey.defaultLayout) == nil)
+        #expect(defaults.object(forKey: PreferenceKeys.rowInspectorJsonFieldHeight.name) == nil)
     }
 }
